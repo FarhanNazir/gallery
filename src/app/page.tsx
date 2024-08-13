07f024293cc8a258ef8f2b633e2a4c2d78199538
Farhan Nazir <farhannazir2660@gmail.com>
2024-08-13 22:48:33 +0800

    change picture url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 
 const mockURLs = [
-  "https://utfs.io/f/cfe01283-d543-48bd-8c7b-1756c3e991e6-lcwl2v.webp",
-  "https://utfs.io/f/436dc575-2602-4f40-81b3-ee2a90a89154-c1hglt.drawio.png",
-  "https://utfs.io/f/30426848-c3fe-4b09-ae2b-62247fa02eae-c1hglt.png",
+  "https://utfs.io/f/74263fb5-a59b-4029-9949-db282b21de66-i2dt9n.jpg",
+  "https://utfs.io/f/42d213ee-3fb0-40c2-bc25-2787f10bb343-gx6p9s.png",
+  "https://utfs.io/f/51ccd07d-3678-416c-a539-167357c8478c-9rtiz7.jpg",
+  "https://utfs.io/f/c2bcda42-5660-4298-8694-1665d71728a0-8ehpaw.jpg",
 ];
 
 const mockImages = mockURLs.map((url, index) => ({
@@ -14,9 +15,9 @@ const mockImages = mockURLs.map((url, index) => ({
 export default function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap">
+      <div className="flex flex-wrap gap-4">
         {mockImages.map((image) => (
-          <div key={image.id} className="w-1/2 p-4">
+          <div key={image.id} className="w-48">
             <img src={image.url} />
           </div>
         ))}
