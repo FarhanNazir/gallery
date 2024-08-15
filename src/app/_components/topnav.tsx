e8fdc0e8b1abbacc5a23806d6e571d38486a3a06
Farhan Nazir <farhannazir2660@gmail.com>
2024-08-15 18:04:41 +0800

    change upload image

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -5,6 +5,25 @@ import { useRouter } from "next/navigation";
 
 import { UploadButton } from "~/utils/uploadthing";
 
+export function UploadSVG() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+      />
+    </svg>
+  );
+}
+
 export function TopNav() {
   const router = useRouter();
 
@@ -17,9 +36,33 @@ export function TopNav() {
         </SignedOut>
         <SignedIn>
           <UploadButton
+            appearance={{
+              button:
+                "ut-ready:bg-none ut-uploading:cursor-not-allowed bg-none bg-black er:bg-none",
+              container:
+                "w-12 flex-row rounded-md border-cyan-300 bg-none mx-2 ",
+              allowedContent: "hidden",
+            }}
             endpoint="imageUploader"
             onClientUploadComplete={() => {
               router.refresh();
+            }}
+            content={{
+              button({ ready }) {
+                if (ready)
+                  return (
+                    <div>
+                      <UploadSVG />
+                    </div>
+                  );
+
+                return "Getting ready...";
+              },
+              allowedContent({ ready, fileTypes, isUploading }) {
+                if (!ready) return "";
+                if (isUploading) return "";
+                return ``;
+              },
             }}
           />
 
