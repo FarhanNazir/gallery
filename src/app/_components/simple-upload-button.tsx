2cca58893496bdf372f6d715c7f85103f1228cad
Farhan Nazir <farhannazir2660@gmail.com>
2024-08-18 04:58:55 +0800

    add rate limit

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -88,6 +88,11 @@ export function SimpleUploadButton() {
         },
       );
     },
+    onUploadError(error) {
+      posthog.capture("upload-error", { error });
+      toast.dismiss("upload-begin");
+      toast.error("upload failed");
+    },
     onClientUploadComplete() {
       toast.dismiss("upload-begin");
       toast("Upload complete");
